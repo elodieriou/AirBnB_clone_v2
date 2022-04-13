82aabd283cd733bf48e6dbd7f34b464bf3bb99b9
Elodie Riou <elodie.riou@hotmail.com>
2022-04-13 09:39:14 +0200

    modify style

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -13,7 +13,7 @@ from sqlalchemy.orm import scoped_session
 from os import getenv
 
 
-class DBStorage():
+class DBStorage:
     """"""
     __engine = None
     __session = None
@@ -21,11 +21,11 @@ class DBStorage():
     def __init__(self):
         """"""
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                               .format(getenv('HBNB_MYSQL_USER'),
-                                       getenv('HBNB_MYSQL_PWD'),
-                                       getenv('HBNB_MYSQL_HOST'),
-                                       getenv('HBNB_MYSQL_DB'),
-                                       pool_pre_ping=True))
+                                      .format(getenv('HBNB_MYSQL_USER'),
+                                              getenv('HBNB_MYSQL_PWD'),
+                                              getenv('HBNB_MYSQL_HOST'),
+                                              getenv('HBNB_MYSQL_DB'),
+                                              pool_pre_ping=True))
     if getenv('HBNB_ENV') == 'test':
         Base.metadata.drop_all(bind=self.__engine)
 
