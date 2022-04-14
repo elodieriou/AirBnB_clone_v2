0961303bb59cf0092de232a49b8514aa25a6b6c0
Elodie Riou <elodie.riou@hotmail.com>
2022-04-14 21:37:12 +0200

    add test docstrings

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -4,6 +4,7 @@ This module contains unit tests for tha database storage.
 """
 import unittest
 import pycodestyle
+from models.engine.db_storage import DBStorage
 
 
 class TestDBStorage(unittest.TestCase):
@@ -13,26 +14,18 @@ class TestDBStorage(unittest.TestCase):
         """Test that we conform to pycodestyle."""
         style = pycodestyle.StyleGuide(quiet=True)
         result = style.check_files(['models/engine/db_storage.py'])
-        self.assertEqual(result.total_errors, 1,
+        self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
-    def test_init_method(self):
-        pass
-
-    def test_all_method(self):
-        pass
-
-    def test_new_method(self):
-        pass
-
-    def test_save_method(self):
-        pass
-
-    def test_delete_method(self):
-        pass
-
-    def test_reload_method(self):
-        pass
+    def test_docstrings(self):
+        """Check for docstrings."""
+        self.assertEqual(len(DBStorage.__doc__), 59)
+        self.assertIsNotNone(DBStorage.__init__.__doc__)
+        self.assertGreater(len(DBStorage.all.__doc__), 0)
+        self.assertIsNotNone(DBStorage.new.__doc__)
+        self.assertLess(len(DBStorage.save.__doc__), 100)
+        self.assertIsNotNone(DBStorage.delete.__doc__)
+        self.assertEqual(len(DBStorage.reload.__doc__), 51)
 
 
 if __name__ == "__main__":
