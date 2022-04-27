4161ab36fe8e7667293f2f20d5367991fad7d0ab
Elodie Riou <elodie.riou@hotmail.com>
2022-04-27 13:33:52 +0200

    add file for compress file before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module defines how to compress before sending"""
+from fabric.api import local
+from os import path
+from time import strftime
+
+
+def do_pack():
+    """This method defines a script that generates a .tgz archive from the
+    contents of the 'web_static' folder of the AirBnB Clone repo"""
+
+    if path.exists("versions/") is False:
+        local("mkdir versions/")
+    else:
+        local("tar -cvzf versions/web_static_{}{}{}{}3357.tgz web_static"
+              .format(strftime("%Y"), strftime("%m"), strftime("%d"),
+                      strftime("%H"), strftime("%M"), strftime("%S")))
