14837a7d5530f90e8d774fb1018d8c289d5dccba
Elodie Riou <elodie.riou@hotmail.com>
2022-04-27 12:50:19 +0200

    add file for prepare our web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# Prepare your web servers
+
+# Install Nginx if not already exist
+if [ ! -x /usr/sbin/nginx ]; then
+  sudo apt-get update
+  sudo apt-get -y install nginx
+fi
+
+# Create folders if not already exist
+if [ ! -d /data/web_static/releases/test/ ]; then
+  sudo mkdir -p /data/web_static/releases/test/
+fi
+if [ ! -d /data/web_static/shared/ ]; then
+  sudo mkdir -p /data/web_static/shared/
+fi
+
+# Create a fake html file to test Nginx configuration
+echo "Deploy web static !" > /data/web_static/releases/test/index.html
+
+# Create a symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership of the /data/ folder to the ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update Nginx configuration
+sed -i '/server_name _;/a \\n\t location /hbnb_static { \n\t\t alias /data/web_static/current/; }' /etc/nginx/sites-available/default
+
+# Restart Nginx
+sudo service nginx restart
