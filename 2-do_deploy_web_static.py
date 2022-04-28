42b86ee79b32a870d67ba30ab81c44b1e1d0ca1c
Elodie Riou <elodie.riou@hotmail.com>
2022-04-28 09:56:27 +0200

    add the if condition in the try

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -31,25 +31,24 @@ def do_pack():
 def do_deploy(archive_path):
     """This method defines a script that distributes an archive to
     a server web"""
-    if path.exists(archive_path) is False:
-        return False
 
     try:
-        file_name = archive_path.split("/")
-        put("{}/{}".format(file_name[0], file_name[1]),
-            "/tmp/{}".format(file_name[1]))
-        del_ext = file_name[1].split(".")
-        run("mkdir -p /data/web_static/releases/{}/".format(del_ext[0]))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(file_name[1], del_ext[0]))
-        run("rm /tmp/{}".format(file_name[1]))
-        run("mv /data/web_static/releases/{}/web_static/*\
-        /data/web_static/releases/{}/".format(del_ext[0], del_ext[0]))
-        run("rm -rf /data/web_static/releases/{}/web_static"
-            .format(del_ext[0]))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(del_ext[0]))
+        if path.exists(archive_path) is True:
+            file_name = archive_path.split("/")
+            put("{}/{}".format(file_name[0], file_name[1]),
+                "/tmp/{}".format(file_name[1]))
+            del_ext = file_name[1].split(".")
+            run("mkdir -p /data/web_static/releases/{}/".format(del_ext[0]))
+            run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+                .format(file_name[1], del_ext[0]))
+            run("rm /tmp/{}".format(file_name[1]))
+            run("mv /data/web_static/releases/{}/web_static/*\
+            /data/web_static/releases/{}/".format(del_ext[0], del_ext[0]))
+            run("rm -rf /data/web_static/releases/{}/web_static"
+                .format(del_ext[0]))
+            run("rm -rf /data/web_static/current")
+            run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+                .format(del_ext[0]))
         return True
 
     except Exception:
