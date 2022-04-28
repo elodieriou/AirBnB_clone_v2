42c1a044a27b9aa56a5bb9bcd9f38a4edd23d7a5
Elodie Riou <elodie.riou@hotmail.com>
2022-04-28 09:39:34 +0200

    without sudo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,11 +2,14 @@
 # Prepare your web servers
 
 # Install Nginx if not already exist
-sudo apt-get update
-sudo apt-get -y install nginx
+if [ ! -x /usr/sbin/nginx ]; then
+  apt-get update
+  apt-get -y install nginx
+fi
 
 # Create folders if not already exist
-mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 
 # Create a fake html file to test Nginx configuration
 echo "Deploy web static !" > /data/web_static/releases/test/index.html
@@ -14,12 +17,12 @@ echo "Deploy web static !" > /data/web_static/releases/test/index.html
 # Create a symbolic link
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Give ownership of the /data/ folder to the ubuntu user and group recursively
+# Give ownership of the /data/ folder to the ubuntu user and group
 chown -R ubuntu:ubuntu /data/
 chgrp -R ubuntu:ubuntu /data/
 
 # Update Nginx configuration
-sudo sed -i '/server_name _;/a \\n\t location /hbnb_static { \n\t\t alias /data/web_static/current/; }' /etc/nginx/sites-available/default
+sed -i '/server_name _;/a \\n\t location /hbnb_static { \n\t\t alias /data/web_static/current/; }' /etc/nginx/sites-available/default
 
 # Restart Nginx
-sudo service nginx start
+service nginx start
