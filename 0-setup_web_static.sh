e35b2c3ed6050d32073624c5c8334758d25a372a
Elodie Riou <elodie.riou@hotmail.com>
2022-04-28 09:32:21 +0200

    add some sudo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,12 +14,12 @@ echo "Deploy web static !" > /data/web_static/releases/test/index.html
 # Create a symbolic link
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Give ownership of the /data/ folder to the ubuntu user and group
+# Give ownership of the /data/ folder to the ubuntu user and group recursively
 chown -R ubuntu:ubuntu /data/
 chgrp -R ubuntu:ubuntu /data/
 
 # Update Nginx configuration
-sed -i '/server_name _;/a \\n\t location /hbnb_static { \n\t\t alias /data/web_static/current/; }' /etc/nginx/sites-available/default
+sudo sed -i '/server_name _;/a \\n\t location /hbnb_static { \n\t\t alias /data/web_static/current/; }' /etc/nginx/sites-available/default
 
 # Restart Nginx
-service nginx start
+sudo service nginx start
