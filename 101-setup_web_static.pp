e03b06fb575c766a871809dbcfa2c8e9d45285c5
Elodie Riou <elodie.riou@hotmail.com>
2022-05-01 13:39:56 +0200

    test

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,33 +1,29 @@
-# Puppet for setup
+
+# puppet manifest preparing a server for static content deployment
 exec { 'apt-get-update':
-  command => 'apt-get -y update',
-  path    => '/usr/bin',
+  command => '/usr/bin/env apt-get -y update',
 }
--> exec {'install':
-  command => 'apt-get -y install nginx',
-  path    => '/usr/bin',
+-> exec {'b':
+  command => '/usr/bin/env apt-get -y install nginx',
 }
--> exec {'create directory':
-  command => 'mkdir -p /data/web_static/releases/test/ /data/web_static/shared/',
-  path    => '/usr/bin',
+-> exec {'c':
+  command => '/usr/bin/env mkdir -p /data/web_static/releases/test/',
 }
--> exec {'create fake html':
-  command => 'echo "Deploy web static with puppet" > /data/web_static/releases/test/index.html',
-  path    => '/usr/bin',
+-> exec {'d':
+  command => '/usr/bin/env mkdir -p /data/web_static/shared/',
 }
--> exec {'create symbolic link':
-  command => 'ln -sf /data/web_static/releases/test /data/web_static/current',
-  path    => '/usr/bin',
+-> exec {'e':
+  command => '/usr/bin/env echo "Puppet x Holberton School" > /data/web_static/releases/test/index.html',
 }
--> exec {'modify configuration':
-  command => 'sed -i "/server_name _;/a location /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default',
-  path    => '/usr/bin',
+-> exec {'f':
+  command => '/usr/bin/env ln -sf /data/web_static/releases/test /data/web_static/current',
 }
--> exec {'restart nginx':
-  command => 'service nginx restart',
-  path    => '/usr/bin',
+-> exec {'h':
+  command => '/usr/bin/env sed -i "/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default',
 }
--> exec {'give ownerships':
-  command => 'chown -R ubuntu:ubuntu /data',
-  path    => '/usr/bin',
+-> exec {'i':
+  command => '/usr/bin/env service nginx restart',
+}
+-> exec {'g':
+  command => '/usr/bin/env chown -R ubuntu:ubuntu /data',
 }
