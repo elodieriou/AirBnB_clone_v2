73da0af7ad3db1395f3c046845a0b5cf81800f01
Elodie Riou <elodie.riou@hotmail.com>
2022-05-01 13:34:34 +0200

    fix code

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,46 +1,32 @@
-# Puppet for setup
-include stdlib
-
-exec { 'update':
-  command => 'sudo apt-get update',
+exec { 'apt-get-update':
+  command => 'apt-get -y update',
   path    => '/usr/bin',
-} ->
-
-package { 'nginx':
-  ensure  => present,
-  name    => 'nginx',
-} ->
-
-exec { 'create directory':
-  command => 'mkdir -p /data/web_static/releases/test /data/web_static/shared',
+}
+-> exec {'install':
+  command => 'apt-get -y install nginx',
   path    => '/usr/bin',
-} ->
-
-file { 'create fake html':
-  ensure  => 'present',
-  path    => '/data/web_static/releases/test/index.html',
-  content => 'Deploy web static !',
-} ->
-
-file { 'create symbolic link':
-  ensure  => 'link',
-  path    => '/data/web_static/current',
-  force   => true,
-  target  => '/data/web_static/releases/test',
-} ->
-
-file_line { 'add alias':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'server_name _;',
-  line   => 'location /hbnb_static { alias /data/web_static/current/; }' /etc/nginx/sites-available/default',
-} ->
-
-service { 'nginx':
-  ensure  => running,
-} ->
-
-exec { 'give ownerships':
+}
+-> exec {'create directory':
+  command => 'mkdir -p /data/web_static/releases/test/ /data/web_static/shared/',
+  path    => '/usr/bin',
+}
+-> exec {'create fake html':
+  command => 'echo "Deploy web static with puppet" > /data/web_static/releases/test/index.html',
+  path    => '/usr/bin',
+}
+-> exec {'create symbolic link':
+  command => 'ln -sf /data/web_static/releases/test /data/web_static/current',
+  path    => '/usr/bin',
+}
+-> exec {'modify configuration':
+  command => 'sed -i "/server_name _;/a location /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default',
+  path    => '/usr/bin',
+}
+-> exec {'restart nginx':
+  command => 'service nginx restart',
+  path    => '/usr/bin',
+}
+-> exec {'give ownerships':
   command => 'chown -R ubuntu:ubuntu /data',
   path    => '/usr/bin',
 }
